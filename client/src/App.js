cf127ee0c50942436b09987804463abac39e85a1
HoanDo <sieunhannhenkutesn@gmail.com>
2020-09-08 23:19:23 +0700

    update challenge

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Round from './Round'
 import {round_one} from './Challenge'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Round challenge = {round_one}/>
+        
       </header>
     </div>
   );
--- client/src/Round.js
+++ client/src/Round.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import{Block,Container} from './styled'
-
+import Lottie from 'lottie-react-web'
+import success from './success.json';
 
 export default (props) => {
     const [challenge,setChallenge] = React.useState(props.challenge)
+    const [win, setWin] = React.useState(false);
     const n = challenge.length;
     const m = challenge[0].length;
+    const checkWin = () => {
+        let flag = true;
+        for (let i = 0; i < n; i++){
+            for(let j = 0; j < m; j++){
+                if(challenge[i][j] == 0){
+                    flag = false;
+                }
+            }
+        }
+        return flag;
+    }
     const handleBlock = (row,col) =>{
       challenge[row][col] = !challenge[row][col]
         if(row > 0 && row < n - 1&& col > 0 && col < m - 1){
@@ -14,43 +27,46 @@ export default (props) => {
             challenge[row + 1][col] = !challenge[row + 1][col]
             challenge[row - 1][col] = !challenge[row - 1][col]
         }
-        if(row == 0  && col > 0 && col < m - 1){
+        if(row === 0  && col > 0 && col < m - 1){
             challenge[row][col - 1] = !challenge[row][col - 1]
             challenge[row][col + 1] = !challenge[row][col + 1]
             challenge[row + 1][col] = !challenge[row + 1][col]
         }
-        if(row == n - 1  && col > 0 && col < m - 1){
+        if(row === n - 1  && col > 0 && col < m - 1){
             challenge[row][col - 1] = !challenge[row][col - 1]
             challenge[row][col + 1] = !challenge[row][col + 1]
             challenge[row - 1][col] = !challenge[row - 1][col]
         }
-        if(col == 0 && row > 0 && row < n  - 1){
+        if(col === 0 && row > 0 && row < n  - 1){
             challenge[row + 1][col] = !challenge[row + 1][col]
             challenge[row - 1][col] = !challenge[row - 1][col]
             challenge[row][col  + 1] = !challenge[row][col + 1]
         }
-        if(col == m - 1 && row > 0 && row < n  - 1){
+        if(col === m - 1 && row > 0 && row < n  - 1){
             challenge[row + 1][col] = !challenge[row + 1][col]
             challenge[row - 1][col] = !challenge[row - 1][col]
             challenge[row][col  - 1] = !challenge[row][col - 1]
         }
-        if(col == m - 1 && row == n - 1){
+        if(col === m - 1 && row === n - 1){
             challenge[row - 1][col] = !challenge[row - 1][col]
             challenge[row][col - 1] = !challenge[row][col - 1]
         }
-        if(col == 0 && row == 0){
+        if(col === 0 && row === 0){
             challenge[row + 1][col] = !challenge[row + 1][col]
             challenge[row][col + 1] = !challenge[row][col + 1]
         }
-        if(col == 0 && row == n  - 1){
+        if(col === 0 && row === n  - 1){
             challenge[row - 1][col] = !challenge[row - 1][col]
             challenge[row][col + 1] = !challenge[row][col + 1]
         }
-        if(col == m - 1 && row == 0){
+        if(col === m - 1 && row === 0){
             challenge[row + 1][col] = !challenge[row + 1][col]
             challenge[row][col - 1] = !challenge[row][col - 1]
         }
         setChallenge([...challenge])
+        if(checkWin()){
+            setWin(true)
+        }
     }
     const generateBlock = (row, value) => {
      
@@ -63,13 +79,19 @@ export default (props) => {
     
     return(
        <div>
-           {challenge.map((value,index)=>{
-               return(
-                   <Container key={index}>
-                       {generateBlock(index,value)}
-                   </Container>
-               )
-           })}
+          {win ? <Lottie
+          style={{position:"a"}}
+          options={{
+            animationData: success
+            }}
+            /> : challenge.map((value,index)=>{
+                return(
+                    <Container key={index}>
+                        {generateBlock(index,value)}
+                    </Container>
+                )
+            })} 
+
        </div>
     )
 
