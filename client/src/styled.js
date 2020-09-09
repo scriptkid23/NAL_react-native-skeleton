cdb135d01ad83923f85a8dfbeaf401a71a2691f3
HoanDo <sieunhannhenkutesn@gmail.com>
2020-09-09 11:56:25 +0700

    update style

--- client/src/styled.js
+++ client/src/styled.js
@@ -6,7 +6,9 @@ const Block = Styled.button`
   border-radius: 20%;
   width : 2.5rem;
   height : 2.5rem;
-  margin : 5px;
+  margin: 0 0.5rem 1rem 0;
+  
+  
 `
 const Container = Styled.div`
     display : flex;
