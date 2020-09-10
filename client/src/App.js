68c3c014c56747f09a4f15a9c4fc94f223912e17
HoanDo <sieunhannhenkutesn@gmail.com>
2020-09-10 10:11:51 +0700

    Update notes game

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Round challenge = {round_one}/>
         
+        <Round challenge = {round_one}/>
+        <div>
+          <p>
+            Please press the button for color the boxes<br/>
+            You win if all the boxes turn <span style={{color:"#fddb3a"}}>yellow</span>
+          </p>
+        </div>
       </header>
     </div>
   );
