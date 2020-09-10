5067c6dd81a48af870d3accb5a4ef9df1314a050
HoanDo <sieunhannhenkutesn@gmail.com>
2020-09-10 11:40:38 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             Please press the button for color the boxes<br/>
             You win if all the boxes turn <span style={{color:"#fddb3a"}}>yellow</span><br/>
             Number of presses remaining {countDown}
-            {countDown < 0 && <p>You Lost</p>}
+            {countDown < 0 && <p>You Lose 🙂</p>}
           </p>
         </div>
       </header>
