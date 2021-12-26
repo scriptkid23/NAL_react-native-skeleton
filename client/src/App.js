3bafde000444621f7d1a16822fcf18714d25c4a9
HoanDo <sieunhannhenkutesn@gmail.com>
2021-12-26 20:07:36 +0700

    docs: update link whitepaper

--- client/src/App.js
+++ client/src/App.js
@@ -98,7 +98,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {status === "unavailable" && <div>MetaMask not available 😰</div>}
+        {status === "unavailable" && <div>MetaMask not available 😰 please read <a href="https://hoan-do.gitbook.io/whitepaper/" style={{ color: "white" }}>Whitepaper</a></div>}
         {status === "notConnected" && <Button onClick={connect}>Connect</Button>}
         {status === "connecting" && <div>Connecting...</div>}
         {status === "connected" && !data.paused && <PlayGame contract={contract}/>}
